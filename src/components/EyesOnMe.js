125fc7d35ad06eff5d058328eade4a245056f19b
Galen Sho Sato <galen.sato@gmail.com>
2023-01-31 09:19:55 -0500

    Completed Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+
+function focusChange (event) {
+    console.log('Good!')
+}
+
+function blurChange(event) {
+    console.log("Hey! Eyes on me!")
+}
+
+function EyesOnMe() {
+    return (
+        <>
+        <button onFocus={focusChange} onBlur={blurChange} >Eyes on me</button>
+        </>
+    )
+}
+
+export default EyesOnMe
